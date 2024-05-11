de61aa7a525b80f0dca3cd88a5a7878115620b1e
Xilerth <xilerth@gmail.com>
2024-05-11 19:53:27 +0200

    Add font files and import font.css in styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,20 +20,84 @@ export class AppComponent {
       if (!log) {
         return;
       }
-      this.logs = [log];
+      this.logs = [...this.logs, log];
     });
 
     this.logService.enableSaveLog('log');
+    this.logService.enableSaveLog('info');
+    this.logService.enableSaveLog('warn');
+    this.logService.enableSaveLog('error');
+    this.logService.enableSaveLog('table');
 
-    setInterval(() => {
-      this.logService.log({
-        message: 'initialized',
-        log: 'log',
-        color: 'lightPink',
-        weight: 'bold',
-        showTime: true,
-        title: 'AppComponent',
-      });
-    }, 1000);
+    // setInterval(() => {
+    this.logService.log({
+      message: 'Welcome to Logger Library',
+      log: 'log',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: '@Xilerth/Logger Library',
+    });
+    // }, 1000);
+  }
+
+  logInfo() {
+    this.logService.log({
+      message: 'This is an info message',
+      log: 'info',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: 'This could be useful',
+    });
+  }
+
+  logWarn() {
+    this.logService.log({
+      message: 'This is a warning message',
+      log: 'warn',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: 'Be careful!',
+    });
+  }
+
+  logError() {
+    this.logService.log({
+      message: 'This is an error message',
+      log: 'error',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: 'Ups! something went wrong',
+    });
+  }
+
+  logTable() {
+    this.logService.log({
+      message: [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' },
+        { id: 3, name: 'John Smith' },
+      ],
+      log: 'table',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: 'Table of users',
+    });
+  }
+
+  log() {
+    this.logService.log({
+      message:
+        'This is a simple log message, you can log anything you want here!',
+      log: 'log',
+      color: 'lightPink',
+      weight: 'bold',
+      showTime: true,
+      title: 'Simple log',
+    });
   }
 }
